ea1f22f4ae9f38298a5b97ab8ca440567b300f4c
Santeri Saarinen <shs.is.acrux@gmail.com>
2024-12-30 09:12:53 +0200

    Daily

--- scrape.sh
+++ scrape.sh
@@ -2,7 +2,7 @@
 
 echo "Scrape.sh version 0.0001 (c) shS 2024"
 
-exec "source .venv/Scripts/activate"&
+exec source .venv/Scripts/activate&
 
 if [ -f cache.html ]; then
 	echo "Old news found. Deleting..."
