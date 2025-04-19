77f31ab7fba86606ac05de5a1f4bc87a47717ba6
Santeri Saarinen <shs.is.acrux@gmail.com>
2025-04-19 16:27:42 +0300

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-personal project
+## Daily news scraper
+_____________________
+
+Scrapes some popular finnish sites.. poorly.
+
+Installation:
+
+	1. git clone this
+	2. uv sync
+	3. export SCRAPER_DIR="current_directory"
+	4. test it.
+
+-shS '25
--- scrape.sh
+++ scrape.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+#
+# INSTALLATION:
+# 	export SCRAPER_DIR=/where/you/cloned_this...where scraper.py is
+#
 
 echo "Scrape.sh version 0.0002 (c) shS 2024"
+
+if [[ -n $SCRAPER_DIR ]]; then
+	cd $SCRAPER_DIR
+fi
+
+if [[ -f $(pwd)/scraper.py ]]; then
+	echo "We are where we're supposed to be in... in $PWD"
+else
+	exit 1
+fi
 
 if [ -f .venv/Scripts/activate ]; then
 	source .venv/Scripts/activate
 elif [ -f .venv/bin/activate ]; then
 	source .venv/bin/activate
+else
+	exit 1
 fi
 
 if [ -f cache.html ]; then
@@ -25,8 +41,8 @@ git add cache.html
 git commit -m "Daily news push..."
 git push
 
-if [ "$1" != "" ]; then
-	if [ "$1" == "--scheduled" ]; then
+if [[ "$1" != "" ]]; then
+	if [[ "$1" == "--scheduled" ]]; then
 		echo "Pausing for 3 seconds or any key..."
 		read -t 5 -p "_________"
 	fi
