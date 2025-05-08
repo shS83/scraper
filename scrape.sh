39759c1eb4899a8591110d66ffb5fe1694eec4ba
Santeri Saarinen <shs.is.acrux@gmail.com>
2025-05-09 01:12:01 +0300

    scrape change

--- scrape.sh
+++ scrape.sh
@@ -6,32 +6,38 @@
 
 echo "Scrape.sh version 0.0002 (c) shS 2024"
 
-if [[ -n $SCRAPER_DIR ]]; then
-	cd $SCRAPER_DIR
+CDIR=$(pwd)
+SCRAPER_DIR=$SCRAPER_DIR
+
+if [[ $SCRAPER_DIR == "" ]]; then
+	printf 'Please set the ENVIRONMENT variable SCRAPER_DIR'
+	exit 1
 fi
 
-if [[ -f $(pwd)/scraper.py ]]; then
+if [[ -f $SCRAPER_DIR/scraper.py ]]; then
 	echo "We are where we're supposed to be in... in $PWD"
 else
 	exit 1
 fi
 
-if [ -f .venv/Scripts/activate ]; then
-	source .venv/Scripts/activate
-elif [ -f .venv/bin/activate ]; then
-	source .venv/bin/activate
+if [[ -f $SCRAPER_DIR/.venv/Scripts/activate ]]; then
+	source $SCRAPER_DIR/.venv/Scripts/activate
+elif [[ -f $SCRAPER_DIR/.venv/bin/activate ]]; then
+	source $SCRAPER_DIR/.venv/bin/activate
 else
 	exit 1
 fi
 
-if [ -f cache.html ]; then
+if [[ -f $SCRAPER_DIR/cache.html ]]; then
 	echo "Old news found. Deleting..."
-	rm cache.html
+	rm cache.html -f
 else
 	echo "Good news, no old news. Getting latest."
 fi
 
-python scraper.py
+cd $SCRAPER_DIR/
+
+python ./scraper.py
 
 echo
 echo "Committing changes..."
