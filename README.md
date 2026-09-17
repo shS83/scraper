ef26d56898d1d2c1ee7d53e04e749b1a347fa458
Santeri 'shS' Saarinen <shs.is.acrux@gmail.com>
2026-09-17 12:12:27 +0300

    Edited pelaaja-section of Scraper with some exclusions.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Installation:
 Warning, the git push action in scrape.sh won't work if you don't fork this repository.
 
 	1. git clone https://www.github.com/shS83/scraper.git; cd scraper
+    2. uv venv; source .venv/bin/activate
 	2. uv sync
 	3. SCRAPER_DIR="$(pwd)"; export SCRAPER_DIR           # make sure you're in the same directory as scrape.sh
 	4. ./scrape.sh
--- scraper.py
+++ scraper.py
@@ -147,6 +147,8 @@ def scrape(url: str):
 			if "image/svg+xml" in ugly[0] or "Episodi.fi" in ugly[0]:
 				continue
 			prettier = ugly[0].replace("Uutinen | ", "")
+			if prettier == "Pelaaja.fi" or prettier == "Como" or prettier == "":
+				continue
 			data_line = {"title": prettier, "href": s["href"]}
 			if data_line not in to_return:
 				to_return.append(data_line)
